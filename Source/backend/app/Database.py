540c642f70702f7889455c6ffa12da24610457d4
Jingyuan Tu <jingyuantu@outlook.com>
2020-09-08 17:24:47 +1000

    Refactoring

---
+++ Source/backend/app/Database.py
@@ -0,0 +1,263 @@
+import pymongo
+import datetime
+
+
+# A class for interacting with database.
+class Database:
+    def __init__(self, addr, username, password):
+        self.addr = addr
+        self.username = username
+        self.password = password
+        self.client = None
+        self.db = None
+
+    # Database Lifecycles
+    def connect(self):
+        self.client = pymongo.MongoClient(
+            self.addr, username=self.username, password=self.password)
+        self.db = self.client['hyperlynkdb']
+
+    def init(self):
+        if self.region_exist('Awaiting Allocation'):
+            self.insert_region('Awaiting Allocation')
+
+    def disconnect(self):
+        return
+
+    # Insertion
+    def insert_region(self, name):
+        self.db.regions.insert_one({'name': name})
+
+    # Existance
+    def device_exist(self, uid):
+        return self.db.devices.count_documents({'device_id': uid}) != 0
+
+    def region_exist(self, name):
+        return self.db.regions.count_documents({'name': name}) != 0
+
+    # Counter
+    def count_operating(self):
+        return self.db.devices.count_documents({'status': 'Online'})
+
+    def count_offline(self):
+        return self.db.devices.count_documents({'status': 'Offline'})
+
+    def count_failure(self):
+        return self.db.devices.count_documents({'status': 'Failure'})
+
+    def count_region(self):
+        return self.db.regions.count_documents({})
+
+    def count_device(self):
+        return self.db.devices.count_documents({})
+
+    # Lenses and Simple Computations
+    def current_out(self, device, time=-1):
+        return device['telemetries'][time]['current_out'] if device['telemetries'] else 0
+
+    def voltage_out(self, device, time=-1):
+        return device['telemetries'][time]['voltage_out'] if device['telemetries'] else 0
+
+    def current_in(self, device, time=-1):
+        return device["telemetries"][time]["current_in"] if device['telemetries'] else 0
+
+    def voltage_in(self, device, time=-1):
+        return device['telemetries'][time]['voltage_in'] if device['telemetries'] else 0
+
+    def power_out(self, device, time=-1):
+        return self.current_out(device, time) * self.voltage_out(device, time)
+
+    def power_in(self, device, time=-1):
+        return self.current_in(device, time) * self.voltage_in(device, time)
+
+    def regions(self):
+        return self.db.regions.find()
+
+    def devices(self, region_name=None, n_latest_records=1):
+        if region_name:
+            return self.db.devices.find(
+                {'region': region_name},
+                {'telemetries': {'$slice': -n_latest_records}}
+            )
+        else:
+            return self.db.devices.find(
+                {},
+                {'telemetries': {'$slice': -n_latest_records}}
+            )
+    
+    def find_device(self, device_id, n_latest_records=1):
+        return self.db.devices.find_one(
+            {"device_id": device_id},
+            {'telemetries': {'$slice': -n_latest_records}}
+        )
+
+    
+
+    
+
+    # def failure_percentage(self):
+    #     on = self.count_operating()
+    #     off = self.count_offline()
+    #     fail = self.count_failure()
+    #     total = on + off + fail
+
+    #     if total == 0:
+    #         return 0
+    #     else:
+    #         return round(fail/total*100, 2)
+
+
+    # Power
+    def total_power(self, region_name=None):
+        pwr_in = 0
+        pwr_out = 0
+        for d in self.devices(region_name):
+            pwr_in += self.power_in(d)
+            pwr_out += self.power_out(d)
+        return round(pwr_in, 2), round(pwr_out, 2)
+
+    def total_power_by_region(self):
+        region_names = []
+        pwr_ins = []
+        pwr_outs = []
+        for r in self.regions():
+            pwr_in, pwr_out = self.total_power(r['name'])
+            region_names.append(r['name'])
+            pwr_ins.append(round(pwr_in, 2))
+            pwr_outs.append(round(pwr_out, 2))
+        return region_names, pwr_ins, pwr_outs
+
+    def total_power_by_device(self, region_name=None):
+        device_ids = []
+        pwr_ins = []
+        pwr_outs = []
+        for d in self.devices(region_name):
+            device_ids.append(d['device_id'])
+            pwr_ins.append(round(self.power_in(d), 2))
+            pwr_outs.append(round(self.power_out(d), 2))
+        return device_ids, pwr_ins, pwr_outs
+
+    # Details
+    def device_detail_graphs(self, device_id, n_latest_records=1):
+        c_ins = [{"id": "current_in", "data": []}]
+        v_ins = [{"id": "voltage_in", "data": []}]
+        c_outs = [{"id": "current_out", "data": []}]
+        v_outs = [{"id": "voltage_out", "data": []}]
+        p_ins = [{"id": "power_in", "data": []}]
+        p_outs = [{"id": "power_out", "data": []}]
+        dev = self.find_device(device_id, n_latest_records)
+        
+        for t in dev['telemetries']:
+            c_ins[0]["data"].append(
+                {"x": len(c_ins[0]["data"]), "y": t["current_in"]})
+            v_ins[0]["data"].append(
+                {"x": len(v_ins[0]["data"]), "y": t["voltage_in"]})
+            c_outs[0]["data"].append(
+                {"x": len(c_outs[0]["data"]), "y": t["current_out"]})
+            v_outs[0]["data"].append(
+                {"x": len(v_outs[0]["data"]), "y": t["voltage_out"]})
+            p_ins[0]["data"].append(
+                {"x": len(p_ins[0]["data"]), "y": t["current_in"] * t["voltage_in"]})
+            p_outs[0]["data"].append(
+                {"x": len(p_outs[0]["data"]), "y": t["current_out"] * t["voltage_out"]})
+
+        return {
+            "device_id": dev["device_id"],
+            "region": dev["region"],
+            "status": dev["status"],
+            "current_in_graph": c_ins,
+            "voltage_in_graph": v_ins,
+            "power_in_graph": p_ins,
+            "current_out_graph": c_outs,
+            "voltage_out_graph": v_outs,
+            "power_out_graph": p_outs
+        }
+    
+    def insert_event(self, device_id, event_code, event_value):
+        self.db.devices.update_one(
+            {'device_id': device_id},
+            {
+                '$push': {
+                    'events': {
+                        'event_code': event_code,
+                        'event_value': event_value
+                    }
+                }
+            }
+        )
+    
+    def insert_telemetry(self, device_id, current_in, voltage_in, current_out, voltage_out):
+        rec_time = datetime.datetime.utcnow()
+
+        self.db.devices.update_one(
+            {'device_id': device_id},
+            {
+                '$push': {
+                    'telemetries': {
+                        'current_in': current_in,
+                        'voltage_in': voltage_in,
+                        'current_out': current_out,
+                        'voltage_out': voltage_out,
+                        'datetime': rec_time
+                    }
+                },
+                '$set': {
+                    'status': "Online",
+                    'last_modified': rec_time
+                }
+            }
+        )
+    
+    def serialise_events(self, device_id):
+        events = self.db.devices.find_one({'device_id': device_id})['events']
+
+        self.db.devices.update_one(
+            {'device_id': device_id},
+            {
+                '$set': {
+                    'events': []
+                }
+            }
+        )
+        event_stream = ""
+        for e in events:
+            event_stream += e["event_code"] + ","
+            event_stream += str(e["event_value"]) + ","
+
+        return event_stream.rstrip(",")
+
+    def set_device_online(self, device_id):
+        self.db.devices.update_one(
+            {'device_id': device_id},
+            {
+                '$set': {
+                    'status': "Online"
+                }
+            }
+        )
+    
+    def insert_device(self, new_id):
+        self.db.devices.insert_one({
+            'device_id': new_id,
+            'status': "Online",
+            'events': [],
+            'telemetries': [],
+            'region': "Awaiting Allocation",
+            'last_modified': datetime.datetime.utcnow()
+        })
+
+    def assign_device_to_region(self, device_id, region_name):
+        self.db.devices.update(
+            {'device_id': device_id},
+            {
+                '$set': {
+                    'region': region_name
+                }
+            }
+        )
+
+    def server_status(self):
+        return 'Normal'
+
+    def system_health(self):
+        return 'Normal'
--- Source/backend/app/server.py
+++ Source/backend/app/server.py
@@ -1,34 +1,25 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
-import pymongo
-import datetime
 import random
 import string
 import os
+from Database import Database
 
 
+# Getting Environments.
 server_address = os.environ.get("SERVER_ADDRESS")
-
 if not server_address:
     exit(1)
 
-db_address = 'mongodb://' + server_address + ':27017/'
-
-
-# Backend App
 app = Flask(__name__)
 CORS(app)
 
 
-# MongoDB Database
-client = pymongo.MongoClient(
-    db_address, username="hyperlynk", password="OnePurpleParrot")
-db = client['hyperlynkdb']
-
-if db.regions.count_documents({'name': "Awaiting Allocation"}) == 0:
-    db.regions.insert_one({
-        'name': "Awaiting Allocation"
-    })
+# Setting up DB.
+db_address = 'mongodb://' + server_address + ':27017/'
+db = Database(db_address, "hyperlynk", "OnePurpleParrot")
+db.connect()
+db.init()
 
 
 # Generate a random device id.
@@ -41,66 +32,34 @@ def response(msg='', payload={}):
     return jsonify({'Message': str(msg), 'Payload': payload})
 
 
-# Existence
-def device_exists(device_id):
-    return db.devices.count_documents({'device_id': device_id}) != 0
-
-
-def region_exists(region_name):
-    return db.regions.count_documents({'name': region_name}) != 0
-
-
 # Viewing
 @app.route('/overview', methods=['GET'])
 def overview():
-    health = "Normal"
-    operating_counts = db.devices.count_documents({"status": "Online"})
-    offline_counts = db.devices.count_documents({"status": "Offline"})
-    failure_counts = db.devices.count_documents({"status": "Failure"})
-
-    failure_percentage = 0.00
-    if (operating_counts+offline_counts+failure_counts) != 0:
-        failure_percentage = round(
-            failure_counts/(operating_counts+offline_counts+failure_counts)*100, 2)
-
-    power_output_out = round(sum([d["telemetries"][-1]["current_out"] * d["telemetries"][-1]["voltage_out"] if d["telemetries"] else 0
-                              for d in db.devices.find()]), 2)
-    
-    power_output_in = round(sum([d["telemetries"][-1]["current_in"] * d["telemetries"][-1]["voltage_in"] if d["telemetries"] else 0
-                              for d in db.devices.find()]), 2)
-
-    server_status = "Normal"
-    region_counts = db.regions.count_documents({})
-
-    overview = {
+    health = db.system_health()
+    operating_counts = db.count_operating()
+    offline_counts = db.count_offline()
+    failure_counts = db.count_failure()
+    total_counts = operating_counts + offline_counts + failure_counts
+    failure_percentage = 0 if total_counts == 0 else round(failure_counts/total_counts*100, 2)
+    power_in, power_out= db.total_power()
+    server_status = db.server_status()
+    region_counts = db.count_region()
+    return response("Success", {
         "health": health,
         "operating_counts": operating_counts,
         "offline_counts": offline_counts,
         "failure_counts": failure_counts,
         "failure_percentage": str(failure_percentage) + "%",
-        "power_output_out": power_output_out,
-        "power_output_in": power_output_in,
+        "power_output_out": power_out,
+        "power_output_in": power_in,
         "server_status": server_status,
         "region_counts": region_counts,
-    }
-
-    return response("Success", overview)
+    })
 
 
 @app.route('/regions', methods=['GET'])
 def regions():
-    regions = []
-    ins = []
-    outs = []
-    for r in db.regions.find():
-        pwr_output_out = round(sum([d["telemetries"][-1]["current_out"] * d["telemetries"][-1]["voltage_out"] if d["telemetries"] else 0
-                                for d in db.devices.find({"region": r["name"]})]), 2)
-        pwr_output_in = round(sum([d["telemetries"][-1]["current_in"] * d["telemetries"][-1]["voltage_in"] if d["telemetries"] else 0
-                                for d in db.devices.find({"region": r["name"]})]), 2)
-        regions.append(r["name"])
-        outs.append(pwr_output_out)
-        ins.append(pwr_output_in)
-
+    regions, ins, outs = db.total_power_by_region()
     return response("Success", {"Regions": regions, "In": ins, "Out": outs})
 
 
@@ -108,25 +67,10 @@ def regions():
 def panels():
     region_name = request.json['region_name']
 
-    if not region_exists(region_name):
+    if not db.region_exist(region_name):
         return response("Region not found.", [])
 
-    panels = []
-    ins = []
-    outs = []
-    for d in db.devices.find({"region": region_name}):
-        pwr_output_out = 0
-        pwr_output_in = 0
-        if d["telemetries"]:
-            pwr_output_out = d["telemetries"][-1]["current_out"] * \
-                d["telemetries"][-1]["voltage_out"]
-            pwr_output_in = d["telemetries"][-1]["current_in"] * \
-                d["telemetries"][-1]["voltage_in"]
-
-        panels.append(d["device_id"])
-        outs.append(round(pwr_output_out, 2))
-        ins.append(round(pwr_output_in, 2))
-
+    panels, ins, outs = db.total_power_by_device(region_name)
     return response("Success", {"Panels": panels, "In": ins, "Out": outs})
 
 
@@ -134,42 +78,10 @@ def panels():
 def panel_detail():
     device_id = request.json['device_id']
 
-    if not device_exists(device_id):
+    if not db.device_exist(device_id):
         return response("Device doest not exists.")
 
-    device = db.devices.find_one({"device_id": device_id})
-    current_in = [{"id": "current_in", "data": []}]
-    voltage_in = [{"id": "voltage_in", "data": []}]
-    current_out = [{"id": "current_out", "data": []}]
-    voltage_out = [{"id": "voltage_out", "data": []}]
-    power_in = [{"id": "power_in", "data": []}]
-    power_out = [{"id": "power_out", "data": []}]
-
-    for t in device["telemetries"][-60:]:
-        current_in[0]["data"].append(
-            {"x": len(current_in[0]["data"]), "y": t["current_in"]})
-        voltage_in[0]["data"].append(
-            {"x": len(voltage_in[0]["data"]), "y": t["voltage_in"]})
-        current_out[0]["data"].append(
-            {"x": len(current_out[0]["data"]), "y": t["current_out"]})
-        voltage_out[0]["data"].append(
-            {"x": len(voltage_out[0]["data"]), "y": t["voltage_out"]})
-        power_in[0]["data"].append(
-            {"x": len(power_in[0]["data"]), "y": t["current_in"] * t["voltage_in"]})
-        power_out[0]["data"].append(
-            {"x": len(power_out[0]["data"]), "y": t["current_out"] * t["voltage_out"]})
-
-    return response("", {
-        "device_id": device["device_id"],
-        "region": device["region"],
-        "status": device["status"],
-        "current_in_graph": current_in,
-        "voltage_in_graph": voltage_in,
-        "power_in_graph": power_in,
-        "current_out_graph": current_out,
-        "voltage_out_graph": voltage_out,
-        "power_out_graph": power_out
-    })
+    return response("", db.device_detail_graphs(device_id, 30))
 
 
 # Event
@@ -179,21 +91,10 @@ def push_event():
     event_code = request.json['event_code']
     event_value = float(request.json['event_value'])
 
-    if not device_exists(device_id):
+    if not db.device_exist(device_id):
         return "Failed. Device not exists."
 
-    db.devices.update_one(
-        {'device_id': device_id},
-        {
-            '$push': {
-                'events': {
-                    'event_code': event_code,
-                    'event_value': event_value
-                }
-            }
-        }
-    )
-
+    db.insert_event(device_id, event_code, event_value)
     return "Successfuly added."
 
 
@@ -206,47 +107,11 @@ def add_telemetry():
     current_out = float(request.json['current_out'])
     voltage_out = float(request.json['voltage_out'])
 
-    if not device_exists(device_id):
+    if not db.device_exist(device_id):
         return ""
-
-    rec_time = datetime.datetime.utcnow()
-
-    db.devices.update_one(
-        {'device_id': device_id},
-        {
-            '$push': {
-                'telemetries': {
-                    'current_in': current_in,
-                    'voltage_in': voltage_in,
-                    'current_out': current_out,
-                    'voltage_out': voltage_out,
-                    'datetime': rec_time
-                }
-            },
-            '$set': {
-                'status': "Online",
-                'last_modified': rec_time
-            }
-        }
-    )
-
-    events = db.devices.find_one({'device_id': device_id})['events']
-
-    db.devices.update_one(
-        {'device_id': device_id},
-        {
-            '$set': {
-                'events': []
-            }
-        }
-    )
-
-    event_stream = ""
-    for e in events:
-        event_stream += e["event_code"] + ","
-        event_stream += str(e["event_value"]) + ","
-
-    return event_stream.rstrip(",")
+    
+    db.insert_telemetry(device_id, current_in, voltage_in, current_out, voltage_out)
+    return db.serialise_events(device_id)
 
 
 # Device Management
@@ -255,15 +120,8 @@ def register_device():
     device_id = request.json['device_id']
 
     # Device exists in database.
-    if device_exists(device_id):
-        db.devices.update_one(
-            {'device_id': device_id},
-            {
-                '$set': {
-                    'status': "Online"
-                }
-            }
-        )
+    if db.device_exist(device_id):
+        db.set_device_online(device_id)
         return device_id
 
     # Verify the id is legal.
@@ -277,15 +135,7 @@ def register_device():
                 new_id = generate_device_id(8)
                 break
 
-    db.devices.insert_one({
-        'device_id': new_id,
-        'status': "Online",
-        'events': [],
-        'telemetries': [],
-        'region': "Awaiting Allocation",
-        'last_modified': datetime.datetime.utcnow()
-    })
-
+    db.insert_device(new_id)
     return new_id
 
 
@@ -294,21 +144,13 @@ def assign_to_region():
     device_id = request.json['device_id']
     region_name = request.json['region_name']
 
-    if not device_exists(device_id):
+    if not db.device_exist(device_id):
         return response("Device not found.")
 
-    if not region_exists(region_name):
+    if not db.region_exist(region_name):
         return response("Region not found.")
 
-    db.devices.update(
-        {'device_id': device_id},
-        {
-            '$set': {
-                'region': region_name
-            }
-        }
-    )
-
+    db.assign_device_to_region(device_id, region_name)
     return response("Device assignment successful.")
 
 
@@ -318,10 +160,8 @@ def register_region():
     region_name = request.json['region_name']
     print(region_name)
 
-    if not region_exists(region_name):
-        db.regions.insert_one({
-            'name': region_name
-        })
+    if not db.region_exist(region_name):
+        db.insert_region(region_name)
         return response('Region successfully added.')
 
     return response('Region name already exists.')
